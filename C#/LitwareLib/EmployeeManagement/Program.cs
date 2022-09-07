fbdc29e9f7136de8aa1832f75bf502648d666349
Abhinav Unniyattil <abhinavunniyattil00@gmail.com>
2022-09-07 15:58:51 +0530

    Added Q1 of Assg3 to existing soln. Also added interactivity

--- C#/LitwareLib/EmployeeManagement/Program.cs
+++ C#/LitwareLib/EmployeeManagement/Program.cs
@@ -8,19 +8,88 @@ namespace EmployeeManagement
     {
         static void Main()
         {
-            Console.WriteLine("Enter the number of employees to be entered :");
-            int numOfEmployees = int.Parse(Console.ReadLine());
-            for (int i = 0; i < numOfEmployees; i++)
+            int option;
+            //Interactive loop to choose what type of employee is to be added by user
+            do
             {
-                Console.WriteLine("Enter the employee number below :");
-                int employeeNumber = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter the name of the employee below :");
-                string employeeName = Console.ReadLine();
-                Console.WriteLine("Enter the salary below :");
-                double employeeSalary = double.Parse(Console.ReadLine());
-                Employee emp1 = new Employee(employeeNumber, employeeName, employeeSalary);
-                emp1.CalculateSalary();
-                emp1.DisplayGrossSalary();
+                Console.WriteLine("What type of Employee would you like to add?\nSelect a number from below :\n" +
+                                  "1. Employee\n" +
+                                  "2. Manager\n" +
+                                  "3. MarketingExecutive\n");
+                option = int.Parse(Console.ReadLine());
+                //validation
+                if (option != 1 && option != 2 && option != 3)
+                {
+                    Console.WriteLine("Enter a Valid option.\n");
+                }
+            } while (option != 1 && option != 2 && option != 3);
+            switch (option)
+            {
+                case 1:
+                    //case for entering employee data
+                    Console.WriteLine("Enter the number of employees to be entered :");
+                    int numOfEmployees = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < numOfEmployees; i++)
+                    {
+                        Console.WriteLine("Enter the employee number below :");
+                        int employeeNumber = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the name of the employee below :");
+                        string employeeName = Console.ReadLine();
+                        Console.WriteLine("Enter the salary below :");
+                        double employeeSalary = double.Parse(Console.ReadLine());
+                        Employee emp1 = new Employee(employeeNumber, employeeName, employeeSalary);
+                        emp1.CalculateSalary();
+                        Console.WriteLine("----------");
+                        emp1.DisplayGrossSalary();
+                        Console.WriteLine("----------");
+                        emp1.PrintDetailsOfEmployee();
+                    }
+                    break;
+                case 2:
+                    //case to add manager data
+                    Console.WriteLine("Enter the number of Managers to be entered :");
+                    int numOfManagers = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < numOfManagers; i++)
+                    {
+                        Console.WriteLine("Enter the Manager's employee number below :");
+                        int employeeNumber = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the name of the Manager below :");
+                        string employeeName = Console.ReadLine();
+                        Console.WriteLine("Enter the salary below :");
+                        double employeeSalary = double.Parse(Console.ReadLine());
+                        Manager manager1 = new Manager(employeeNumber, employeeName, employeeSalary);
+                        manager1.CalculateSalary();
+                        Console.WriteLine("----------");
+                        manager1.DisplayGrossSalary();
+                        Console.WriteLine("----------");
+                        manager1.PrintDetailsOfEmployee();
+
+                    }
+                    break;
+                case 3:
+                    //case to add marketing exec data
+                    Console.WriteLine("Enter the number of Marketing Executive to be entered :");
+                    int numOfExecutives = int.Parse(Console.ReadLine());
+                    for (int i = 0; i < numOfExecutives; i++)
+                    {
+                        Console.WriteLine("Enter the Marketing Executive's employee number below :");
+                        int employeeNumber = int.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the name of the Marketing Executive below :");
+                        string employeeName = Console.ReadLine();
+                        Console.WriteLine("Enter the salary below :");
+                        double employeeSalary = double.Parse(Console.ReadLine());
+                        Console.WriteLine("Enter the kilometers travelled below :");
+                        double kilometersTravelled = double.Parse(Console.ReadLine());
+                        MarketingExecutive manager1 = new MarketingExecutive(employeeNumber, employeeName, employeeSalary, kilometersTravelled);
+                        manager1.CalculateSalary();
+                        Console.WriteLine("----------");
+                        manager1.DisplayGrossSalary();
+                        Console.WriteLine("----------");
+                        manager1.PrintDetailsOfEmployee();
+
+                    }
+                    break;
+
             }
         }
     }
--- C#/LitwareLib/LitwareLib/Class1.cs
+++ C#/LitwareLib/LitwareLib/Class1.cs
@@ -2,15 +2,22 @@
 
 namespace LitwareLib
 {
-    public class Employee
+    //cerating an interface to print all detail of an employee
+    interface IPrintable
+    {
+        void PrintDetailsOfEmployee();
+
+    }
+    public class Employee : IPrintable
     {
         private int EmpNo;
         private string EmpName;
         private double salary;
         double HRA, TA, DA, PF, TDS;
         double NetSalary;
-        double GrossSalary;
+        public double GrossSalary;
 
+        //defining the properties of private members
         public int empNo
         {
             get;
@@ -27,55 +34,118 @@ namespace LitwareLib
             get;
             set;
         }
+
+        //parametrized constructor
         public Employee(int EmpNo, string EmpName, double salary)
         {
             this.EmpNo = EmpNo;
             this.EmpName = EmpName;
-            this.salary = salary;
+            Salary = salary;
         }
 
-        public void CalculateSalary()
+        //func to calculate the gross salary and net salary
+        public virtual void CalculateSalary()
         {
-            if (salary < 5000)
+            if (Salary < 5000)
             {
-                HRA = 0.1 * salary;
-                TA = 0.05 * salary;
-                DA = 0.15 * salary;
+                HRA = 0.1 * Salary;
+                TA = 0.05 * Salary;
+                DA = 0.15 * Salary;
             }
-            else if (salary < 10000)
+            else if (Salary < 10000)
             {
-                HRA = 0.15 * salary;
-                TA = 0.1 * salary;
-                DA = 0.2 * salary;
+                HRA = 0.15 * Salary;
+                TA = 0.1 * Salary;
+                DA = 0.2 * Salary;
             }
-            else if (salary < 15000)
+            else if (Salary < 15000)
             {
-                HRA = 0.2 * salary;
-                TA = 0.15 * salary;
-                DA = 0.25 * salary;
+                HRA = 0.2 * Salary;
+                TA = 0.15 * Salary;
+                DA = 0.25 * Salary;
             }
-            else if (salary < 20000)
+            else if (Salary < 20000)
             {
-                HRA = 0.25 * salary;
-                TA = 0.2 * salary;
-                DA = 0.3 * salary;
+                HRA = 0.25 * Salary;
+                TA = 0.2 * Salary;
+                DA = 0.3 * Salary;
             }
             else
             {
-                HRA = 0.3 * salary;
-                TA = 0.25 * salary;
-                DA = 0.35 * salary;
+                HRA = 0.3 * Salary;
+                TA = 0.25 * Salary;
+                DA = 0.35 * Salary;
             }
 
-            GrossSalary = salary + HRA + TA + DA;
+            GrossSalary = Salary + HRA + TA + DA;
             PF = 0.1 * GrossSalary;
             TDS = 0.18 * GrossSalary;
             NetSalary = GrossSalary - (PF + TDS);
         }
+
+        //func to display gross salary
         public void DisplayGrossSalary()
         {
             Console.WriteLine("The Gross Salary is {0}", GrossSalary);
         }
+
+        //defining the method of the interface
+        public void PrintDetailsOfEmployee()
+        {
+            Console.WriteLine("Name of Employee : {0}", EmpName);
+            Console.WriteLine("Employee Number of Employee : {0}", EmpNo);
+            Console.WriteLine("Base Salary of Employee : {0}", Salary);
+            Console.WriteLine("Gross Salary of Employee : {0}", GrossSalary);
+            Console.WriteLine("Net Salary of Employee : {0}", NetSalary);
+        }
+
+    }
+
+    public class Manager : Employee
+    {
+        private double _petrolAllowance;
+        private double _foodAllowance;
+        private double _otherAllowance;
+
+        //parametrized constructor
+        public Manager(int EmpNo, string EmpName, double salary) : base(EmpNo, EmpName, salary)
+        {
+
+        }
+
+        //overriding calculate salary method from base class
+        public override void CalculateSalary()
+        {
+            base.CalculateSalary();
+
+            _petrolAllowance = 0.08 * Salary;
+            _foodAllowance = 0.13 * Salary;
+            _otherAllowance = 0.03 * Salary;
+
+            GrossSalary = GrossSalary + _petrolAllowance + _foodAllowance + _otherAllowance;
+        }
+    }
+
+    public class MarketingExecutive : Manager
+    {
+        private double _kilometeresTravelled;
+        private double _tourAllowance;
+        private int _telephoneAllowance = 1000;
+
+        //parametrized constructor
+        public MarketingExecutive(int EmpNo, string EmpName, double salary, double kilometeresTravelled) : base(EmpNo, EmpName, salary)
+        {
+            this._kilometeresTravelled = kilometeresTravelled;
+        }
+
+        //overriding func from base class
+        public override void CalculateSalary()
+        {
+            base.CalculateSalary();
+            _tourAllowance = 5 * _kilometeresTravelled;
+            GrossSalary = GrossSalary + _telephoneAllowance + _tourAllowance;
+        }
+
 
     }
 }
