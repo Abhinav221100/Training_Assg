3e9c1e4abc67823efcf9d22b5e925677395d4da5
Abhinav Unniyattil <abhinavunniyattil00@gmail.com>
2022-09-07 17:39:24 +0530

    Adding Assignment 3

---
+++ C#/Assignment3/Assignment3/Class1.cs
@@ -0,0 +1,66 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Assignment3
+{
+    interface ICloneable
+    {
+        public int[] clone(int size, int[] stack);
+    }
+
+    public class MyStack : ICloneable
+    {
+        public int[] stack;
+        public int top;
+        int size;
+
+        public MyStack(int size)
+        {
+            this.top =  -1;
+            this.size = size;
+            this.stack = new int[size]; 
+        }
+
+        public void push(int data)
+        {
+            if(top+1>=size)
+            { 
+                Console.WriteLine("----------");
+                Console.WriteLine("WARNING : Buffer Overflow");
+                Console.WriteLine("----------");
+                return;
+            }
+            top = top + 1;
+            stack[top] = data;
+            Console.WriteLine("----------");
+            Console.WriteLine("Push operation successfull");
+            Console.WriteLine("----------");
+        }
+
+        public int pop()
+        {
+            if(top == -1)
+            {
+                Console.WriteLine("----------");
+                Console.WriteLine("WARNING : Buffer Underflow");
+                Console.WriteLine("----------");
+                return -99999;
+            }
+            int value = stack[top];
+            top = top - 1;
+            Console.WriteLine("----------");
+            Console.WriteLine("Pop operation successfull");
+            Console.WriteLine("----------");
+            return value;
+        }
+
+        public int[] clone(int size, int[] stack)
+        {
+            MyStack stack2 = new MyStack(size);
+            stack2.stack = stack;
+            Console.WriteLine("----------");
+            Console.WriteLine("Cloning operation successfull");
+            Console.WriteLine("----------");
+            return stack2.stack;
+        }
+    }
+}
---
+++ C#/Assignment3/ConsoleApp/Program.cs
@@ -0,0 +1,77 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+using Assignment3;
+
+namespace Console_Assg3
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Enter the size of the stack below :");
+            int sizeOfStack = int.Parse(Console.ReadLine());
+            MyStack stack1 = new MyStack(sizeOfStack);
+            int choice;
+
+            do
+            {
+                do
+                {
+                    Console.WriteLine("Choose one of the Operations from below (Choose a number) :\n" +
+                                      "1. Push\n" +
+                                      "2. Pop\n" +
+                                      "3. Clone\n" +
+                                      "4. Quit\n");
+                    choice = int.Parse(Console.ReadLine());
+                    if (choice != 1 && choice != 2 && choice != 3 && choice != 4)
+                    {
+                        Console.WriteLine("Enter a Valid option.");
+                    }
+                    if(choice== 4)
+                    {
+                        break;
+                    }
+
+                } while (choice != 1 && choice != 2 && choice != 3);
+
+                if(choice == 4)
+                {
+                    break;
+                }
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Enter a number to push onto stack :");
+                        int data = int.Parse(Console.ReadLine());
+                        stack1.push(data);
+                        break;
+                    case 2:
+                        int value = stack1.pop();
+                        if(value == -99999)
+                        {
+                            break;
+                        }
+                        Console.WriteLine("The value popped is {0}\n",value);
+                        break;
+                    case 3:
+                        int[] clone = stack1.clone(sizeOfStack, stack1.stack);
+                        int temp = stack1.top;
+                        Console.WriteLine("----------");
+                        Console.WriteLine("The Cloned Stack is as follows :");
+                        while(temp > -1)
+                        {
+                            Console.WriteLine(clone[temp]);
+                            temp = temp - 1;
+                        }
+                        Console.WriteLine("----------");
+                        break;
+                    case 4:
+                        break;
+                }
+            } while (choice == 1 || choice == 2 || choice == 3);
+            
+            
+        }
+
+    }
+}
